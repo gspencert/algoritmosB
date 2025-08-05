3c151d15345cbd9629100840292a908b58e95196
Guilherme Spencer <spencer24guilherme@gmail.com>
2025-08-05 01:01:54 -0300

    Add files via upload

---
+++ revisao ling C/revisao C/programa1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+int main(){
+    system("clear"); // comando para limpar a tela no linux
+    printf("boa noite\n");
+    return 0;
+}
+// gcc programName.c -o roda
+// ./roda
