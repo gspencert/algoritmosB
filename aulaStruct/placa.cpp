0ddd9a9b21f7ea4586ac906c83bd12aa66bdf01b
Guilherme Spencer <spencer24guilherme@gmail.com>
2025-08-05 01:07:52 -0300

    correcao algoritmo placa

--- aulaStruct/placa.cpp
+++ aulaStruct/placa.cpp
@@ -6,15 +6,21 @@
 using namespace std;
 
 int main(){
-    cout << "exemplo de placa: ";
+
     string placa;
     string vetor[TAM];
 
-    for(i=0;i<TAM;i++){
-    cout << "insira uma placa";
-    cin >> placa;
+    for(int i = 0; i < TAM ; i++ ){
+        cout << "insira uma placa";
+        cin >> placa;
 
-    vetor[i] = placa;
+        vetor[i] = placa;
     }
+
+    for(int i = 0 ; i < TAM ; i++){
+        cout << "placa: " << placa << "\n";
+    }
+
+    cout << "\n";
     return 0;
 }
