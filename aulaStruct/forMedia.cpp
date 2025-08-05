df9354206f3382133a1235062af0c6719637e6c1
Guilherme Spencer <spencer24guilherme@gmail.com>
2025-08-05 19:19:26 -0300

    notas | For

---
+++ aulaStruct/forMedia.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int i;
+    float nota[3], media=0;
+    
+    for (i=0;i<3;i++){
+        cout << "digite a nota " << i+1 << ": ";
+        cin >> nota[i];
+        media += nota[i];
+    }
+
+    media /= 3;
+
+    cout << "a media eh " << media; 
+    cout << ("\n");
+    
+    return 0;
+}
